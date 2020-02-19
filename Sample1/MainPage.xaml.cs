1d7a500a6cbff722148ca72094e59d7f28de27a0
jbehrend4 <Izanagi803>
2020-02-18 19:32:41 -0600

    Initial check-in of module Sample1

---
+++ Sample1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        string enteredText = "";
+        string outputText = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        void typePeriod(object sender, EventArgs e)
+        {
+            enteredText += ".";
+        }
+        void typeDash(object sender, EventArgs e)
+        {
+            enteredText += "-";
+        }
+        void typeSpace(object sender, EventArgs e)
+        {
+            char text = Morse.MorseCoder(enteredText);
+            outputText += text;
+            enteredText = "";
+            output.Text = outputText;
+        }
+    }
+}
